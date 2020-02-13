4cca5c2bef212f53d2c69e0d5c25471aed54dd30
Johannes <j.rosvik@gmail.com>
2020-02-13 19:31:04 +0100

    Add website content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-$(document).ready(function(){
-	$('.parallax').parallax();
-});
+document.getElementById("copyright-year").outerHTML =
+  new Date().getYear() + 1900;
+
+var startTime, time;
+var duration = 4000;
+var startX = 0,
+  endX = 200;
+var obj = document.getElementsByClassName("canvas")[0];
+var lettering = document.getElementsByClassName("lettering")[0];
+
+var run = function() {
+  time = new Date().getTime() - startTime;
+  time = time / duration;
+  requestAnimationFrame(run);
+
+  var r = 50;
+  var x = Math.floor(r * Math.cos(time));
+  var y = Math.floor(r * Math.sin(time) - window.pageYOffset);
+  var heightoffset = 0.35 * window.innerHeight - 0.5 * obj.clientWidth;
+
+  setPos(x, y, heightoffset);
+};
+
+var setPos = function(x, y, heightoffset) {
+  obj.style["background-position"] =
+    "0 " + heightoffset + "px, " + x + "px " + y + "px";
+};
+
+startTime = new Date().getTime();
+run();
