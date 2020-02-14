cfa13f69ebd06c394dc1825c4787de329a1c5c1b
Johannes <j.rosvik@gmail.com>
2020-02-14 12:04:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,26 +7,32 @@ var startX = 0,
   endX = 200;
 var obj = document.getElementsByClassName("canvas")[0];
 var lettering = document.getElementsByClassName("lettering")[0];
+var heightoffset = 0;
 
 var run = function() {
   time = new Date().getTime() - startTime;
   time = time / duration;
-  requestAnimationFrame(run);
 
   var r = 50;
-  // var x = Math.floor(r * Math.cos(time));
   var x = 0;
-  // var y = Math.floor(r * Math.sin(time) - window.pageYOffset);
-  var y = 50 * time - window.pageYOffset;
-  var heightoffset = 0.35 * window.innerHeight - 0.5 * obj.clientWidth;
+  var scrollpos = window.pageYOffset;
+  var y = 50 * time - scrollpos;
+  heightoffset =
+    0.5 * window.innerHeight - 0.5 * obj.clientWidth + 0.8 * scrollpos;
 
+  requestAnimationFrame(run);
   setPos(x, y, heightoffset);
 };
 
 var setPos = function(x, y, heightoffset) {
   obj.style["background-position"] =
-    "0 " + window.pageYOffset * 0.9 + "px, " + x + "px " + y + "px";
+    "0 " + heightoffset + "px, " + x + "px " + y + "px";
 };
 
 startTime = new Date().getTime();
+
 run();
+
+// setInterval(() => {
+//   run();
+// }, 1);
